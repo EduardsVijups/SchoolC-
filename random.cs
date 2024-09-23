e0d10d09e3fd5e70abe7ea717fdff0debee3e07f
EduardsVijups <48444373+EduardsVijups@users.noreply.github.com>
2024-09-23 13:28:53 +0300

    Update random.cs

--- random.cs
+++ random.cs
@@ -1,23 +1,24 @@
-// Online C# Editor for free
-// Write, Edit and Run your C# code using C# Online Compiler
-
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class HelloWorld
+public class Project1
 {
     public static void Main(string[] args)
     {
-        int x = 1;
+        int x = 2;
         int y = 1;
         int score = 0;
         string[] m = {
-            "##########",
-            "#@       #",
-            "#        #",
-            "#        #",
-            "#        #",
-            "#        #",
-            "##########",
+            "# # # # # # # # # #",
+            "# @               #",
+            "#                 #",
+            "#                 #",
+            "#                 #",
+            "#                 #",
+            "# # # # # # # # # #",
         };
         /*for (int j = 0; j < 10; j++) {
             Console.WriteLine(" ");
@@ -41,7 +42,7 @@ public class HelloWorld
         }*/
         draw(m);
         Random rnd = new Random();
-        int randX = rnd.Next(1, 8);
+        int randX = rnd.Next(1, 8)*2;
         int randY = rnd.Next(1, 5);
         run(x, y, randX, randY, score);
     }
@@ -53,7 +54,7 @@ public class HelloWorld
             Console.WriteLine(m[i]);
         }
     }
-    
+
     public static void run(int a, int b, int c, int d, int e)
     {
         int x = a;
@@ -62,74 +63,93 @@ public class HelloWorld
         int randY = d;
         int score = e;
         string[] m = {
-            "##########",
-            "#        #",
-            "#        #",
-            "#        #",
-            "#        #",
-            "#        #",
-            "##########",
+            "# # # # # # # # # #",
+            "#                 #",
+            "#                 #",
+            "#                 #",
+            "#                 #",
+            "#                 #",
+            "# # # # # # # # # #",
         };
         string input;
         input = Console.ReadLine();
-        if (input == "a") {
-            if (x > 1) {
-                x -= 1;
-            } else {
+        if (input == "a")
+        {
+            if (x > 1)
+            {
+                x -= 2;
+            }
+            else
+            {
                 x = 1;
             }
         }
-        if (input == "d") {
-            if (x < 8) {
-                x += 1;
-            } else {
-                x = 8;
+        if (input == "d")
+        {
+            if (x < 17)
+            {
+                x += 2;
+            }
+            else
+            {
+                x = 17;
             }
         }
-        if (input == "w") {
-            if (y > 1) {
+        if (input == "w")
+        {
+            if (y > 1)
+            {
                 y -= 1;
-            } else {
+            }
+            else
+            {
                 y = 1;
             }
         }
-        if (input == "s") {
-            if (y < 5) {
+        if (input == "s")
+        {
+            if (y < 5)
+            {
                 y += 1;
-            } else {
+            }
+            else
+            {
                 y = 5;
             }
         }
-        if (y == randY && x == randX) {
+        if (y == randY && x == randX)
+        {
             score += 1;
             Random rnd = new Random();
-            randX = rnd.Next(1, 8);
+            randX = rnd.Next(2, 8)*2;
             randY = rnd.Next(1, 5);
         }
         for (int i = 0; i < m.Length; i++)
         {
-            if (i == y){
+            if (i == y)
+            {
                 var pp = m[i].ToCharArray();
                 pp[x] = '@';
 
                 var result = new string(pp);
                 m[i] = result;
-                
+
             }
-            if (i == randY){
+            if (i == randY)
+            {
                 var pp = m[i].ToCharArray();
                 pp[randX] = 'X';
 
                 var result = new string(pp);
                 m[i] = result;
-                
-                
+
+
             }
-        
-            
+
+
         }
         draw(m);
-        Console.WriteLine("Score = "+score);
+        Console.WriteLine("Score = " + score);
         run(x, y, randX, randY, score);
     }
 }
